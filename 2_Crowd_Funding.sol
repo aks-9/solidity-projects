f7734f0f3094118b63fabad3620b14c1fc5c9fec
aks-9 <81648775+aks-9@users.noreply.github.com>
2022-01-17 18:05:42 +0530

    Update 2_Crowd_Funding.sol
    Removed Comments

--- 2_Crowd_Funding.sol
+++ 2_Crowd_Funding.sol
@@ -3,8 +3,8 @@ pragma solidity >=0.5.0 < 0.9.0;
 
 contract CrowdFunding{
 
-    //Defining variables
-    mapping(address=>uint) public contributors; //this mapping will tell how much amount given by which address.
+    //State variables
+    mapping(address=>uint) public contributors; 
     address public manager;
     uint public minimumContribution;
     uint public deadline;
@@ -15,62 +15,54 @@ contract CrowdFunding{
     constructor(uint _target,uint _deadline)
     {
         target = _target;
-        deadline = block.timestamp + _deadline; //10sec + 3600sec (60*60)
+        deadline = block.timestamp + _deadline; 
         minimumContribution = 100 wei;
         manager = msg.sender;
     }      
 
-    //PART 1- FOR CONTRIBUTORS
+    //FOR CONTRIBUTORS
 
     //For contributors to send eth.
     function sendEth() public payable{
-        require(block.timestamp < deadline,"Deadline has passed"); //Error message is attached in "".
+        require(block.timestamp < deadline,"Deadline has passed"); 
         require(msg.value >=minimumContribution,"Minimum Contribution is not met");
 
-        if(contributors[msg.sender]==0) //The mapping called 'contributors' will have an 'address' that will be in msg.sender. So if the contributor is sending money for the first time, his address will have zero amount in the mapping. If he's contributed earlier as well then it will not be zero. This will not let anyone gain from multiple transactions with small amount in order to get more voting rights.
+        if(contributors[msg.sender]==0) 
         {
-            noOfContributors++; //increasing it if it's a new contributor.
+            noOfContributors++; 
         }
-        contributors[msg.sender]+=msg.value; // contributors[msg.sender] = contributors[msg.sender] + msg.value;
-
-        //contributors[msg.sender] represents the amount sent by a particular address. If he's an older contributor then we simply add its new contribution represented by 'msg.value' to the previous total contribution represented by contributors[msg.sender]
-        raisedAmount+=msg.value; // raisedAmount = raisedAmount + msg.value; Updating the total raised amount after every contribution.
+        contributors[msg.sender]+=msg.value;         
+        raisedAmount+=msg.value;
     }
 
-
-    //To read the current balance of the contract.
     function getContractBalance() public view returns(uint)
     {
         return address(this).balance;
     }
 
-
-
     function refund() public{
         require(block.timestamp>deadline && raisedAmount<target,"You are not eligible for refund");
-        require(contributors[msg.sender] > 0); //amount contributed must be greater than zero to be a contributor, only then refund can be claimed.
-        address payable user = payable(msg.sender); //creating a variable 'user' of datatype 'address, and have to make it explicitly payable to process refund.
-        user.transfer(contributors[msg.sender]); //Transfering the ether present in the contributors[msg.sender] to 'user'
-        contributors[msg.sender]=0; // setting the amount held by the address of the contributor, in the mapping 'contributors', that is claiming the refund to  zero.
-
+        require(contributors[msg.sender] > 0); 
+        address payable user = payable(msg.sender); 
+        user.transfer(contributors[msg.sender]); 
+        contributors[msg.sender]=0; 
     }
 
 
-    //PART 2- FOR THE MANAGER
+    //FOR THE MANAGER
 
-    struct Request //Creating a structure called 'Request'.
+    struct Request
     {
-        string description; // Reason for withdrawal
-        address payable recipient; // address where amount will be withdrawn.
-        uint value; // Amount asked for withdrwal by manager.
-        bool isCompleted; // Is this request pending for voting or not?
-        uint noOfVoters; //How many voted.
-        mapping(address=>bool) voters; //Each address to be mapped to either Yes or No.
+        string description; 
+        address payable recipient; 
+        uint value; 
+        bool isCompleted; 
+        uint noOfVoters; 
+        mapping(address=>bool) voters; 
     }
     
-    mapping(uint=>Request) public allRequests; //A mapping to log different kinds of requests eg. business, charity, personal etc. This will have many integers from 0 onwards , each with a unique Request structure. So this mapping will have many structures on its indexes.
-    uint public numRequests; //It denotes the index of the mapping 'allRequests'. It's not possible to increment in a mapping like in an array, so numRequests will be used.
-
+    mapping(uint=>Request) public allRequests; 
+    uint public numRequests; 
 
     modifier onlyManger(){
         require(msg.sender==manager,"Only manager can call this function");
@@ -79,11 +71,7 @@ contract CrowdFunding{
 
     function createRequests(string memory _description,address payable _recipient,uint _value) public onlyManger
     {
-        Request storage newRequest = allRequests[numRequests]; // We have used 'storage' keyword here, because whenever we create a structure, and then create a mapping in that structure, then we can't use 'memory' keyword in a function to access that structure.
-        // Here we're creating a new variable called 'newRequest' of type 'Request', which is a structure. Then whatever is in allRequests[0] is going into 'newRequest'. So 'newRequest' is able to make changes in the structure 'Request' for zeroth position,i.e. allRequests[0] of the mapping 'allRequests'. 
-        
-        //In short 'newRequest' will point to a structure 'Request' at the index of 'allRequests' mapping.
-
+        Request storage newRequest = allRequests[numRequests];
         numRequests++;
         newRequest.description = _description;
         newRequest.recipient = _recipient;
@@ -96,19 +84,19 @@ contract CrowdFunding{
     function voteRequest(uint _requestNo) public
     {
         require(contributors[msg.sender] > 0 ,"You must be contributor");
-        Request storage thisRequest = allRequests[_requestNo]; //Creating another structure 'thisRequest' of type 'Request'.
-        require(thisRequest.voters[msg.sender] == false,"You have already voted");// Default value of bool is false.
-        thisRequest.voters[msg.sender] = true; //As voting is done once.
-        thisRequest.noOfVoters++; //incrementing.
+        Request storage thisRequest = allRequests[_requestNo]; 
+        require(thisRequest.voters[msg.sender] == false,"You have already voted");
+        thisRequest.voters[msg.sender] = true; 
+        thisRequest.noOfVoters++; 
     }
 
-    //Checking voting is in favour of a particular request or not, and then transfering money to a particular address.
+    
     function makePayment(uint _requestNo) public onlyManger
     {
         require(raisedAmount >= target);
         Request storage thisRequest = allRequests[_requestNo];
         require(thisRequest.isCompleted == false,"The request has been completed");
-        require(thisRequest.noOfVoters > noOfContributors/2,"Majority does not support"); // checking greater than 50% or not.
+        require(thisRequest.noOfVoters > noOfContributors/2,"Majority does not support"); 
         thisRequest.recipient.transfer(thisRequest.value);
         thisRequest.isCompleted = true;
     }
