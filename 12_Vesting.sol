4a55ccd95c81d60a0607c5d6df245e7f253cdec5
Ashish Kumar Singh <ashishsingh4job@gmail.com>
2022-04-01 14:02:16 +0530

    Create 12_Vesting.sol

---
+++ 12_Vesting.sol
@@ -0,0 +1,47 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract Vesting{
+
+    struct Founder {
+        uint amount;
+        uint maturity;
+        bool paid;
+
+    }
+
+    mapping(address => Founder) public founders;
+    address public admin;
+
+    event addFounderEvent (address _founderEvent, uint _timeToMaturityEvent, string messageEvent);
+
+    constructor(){
+        admin = msg.sender;
+
+    }
+
+    function addFounder(address _founder, uint _timeToMaturity) external payable {
+        require(msg.sender == admin, "Only admin can add a founder.");
+        require(founders[_founder].amount == 0 ,"Founder already exists");
+
+        founders[_founder] = Founder(msg.value, block.timestamp + _timeToMaturity, false);
+
+        emit addFounderEvent(_founder, _timeToMaturity, "Founder added.");
+
+    }
+
+    function withdraw() external returns(bool) {
+        Founder storage founder = founders[msg.sender];
+
+        require(founder.maturity <= block.timestamp, "Vesting period hasn't ended");
+        require(founder.amount > 0);
+        require(founder.paid == false," Already paid.");
+
+        founder.paid = true;
+        payable(msg.sender).transfer(founder.amount);
+
+        return true;
+        
+    }
+
+}
